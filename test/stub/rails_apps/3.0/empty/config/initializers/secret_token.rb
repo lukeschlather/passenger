4de3dfec260898a033e3a7996d06e7a334b9425a
Luke Schlather <luke.schlather@gmail.com>
2013-09-18 10:34:24 -0700

    Update secret_token.rb
    Clear out comment.

--- test/stub/rails_apps/3.0/empty/config/initializers/secret_token.rb
+++ test/stub/rails_apps/3.0/empty/config/initializers/secret_token.rb
@@ -1,7 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
+# Since this is just a test app, we set the secret token to a random value.
 Rails.application.config.secret_token = SecureRandom.hex(128)
